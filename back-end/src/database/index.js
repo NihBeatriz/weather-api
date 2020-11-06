e2bcdf85ec1540a91cb39f81328e87ecf6bdb3f4
Nicole Beatriz <nick_beatriz_@hotmail.com>
2020-11-06 17:36:39 -0300

    "Authentication"

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/weather', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/weather', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true 
+})
 .then(response => console.log('Conected to Database..'))
-.catch(error => console.log('error ->', error.message));
+.catch(error => console.log('error ->', error.message))
 
-module.exports = mongoose;
+module.exports = mongoose
